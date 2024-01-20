649774f6a6ead1da7e106bc44f77d70d7101b63b
Starbucks <iamasmallp@gmail.com>
2024-01-20 18:48:56 +0800

    add Message size test

--- bolt-spot/src/main/java/com/cmex/bolt/spot/api/Unfrozen.java
+++ bolt-spot/src/main/java/com/cmex/bolt/spot/api/Unfrozen.java
@@ -3,6 +3,5 @@ package com.cmex.bolt.spot.api;
 import javolution.io.Struct;
 
 public class Unfrozen extends Struct {
-    public final Signed64 freezeId = new Signed64();
     public final Signed64 amount = new Signed64();
 }
---
+++ bolt-spot/src/test/java/com/cmex/bolt/spot/api/TestMessage.java
@@ -0,0 +1,20 @@
+package com.cmex.bolt.spot.api;
+
+import org.junit.jupiter.api.Test;
+
+public class TestMessage {
+
+    @Test
+    public void testSize(){
+        Message message = new Message();
+        System.out.println(message.size());
+        PlaceOrder placeOrder = new PlaceOrder();
+        System.out.println(placeOrder.size());
+        CancelOrder cancelOrder = new CancelOrder();
+        System.out.println(cancelOrder.size());
+        Unfreeze unfreeze = new Unfreeze();
+        System.out.println(unfreeze.size());
+        Unfrozen unfrozen = new Unfrozen();
+        System.out.println(unfrozen.size());
+    }
+}
