c31fd43a7d4c10c188aeb4e02802313f7370fc71
ly <ly.lazytalent@gmail.com>
2025-06-06 23:19:38 +0400

    简单调整

--- bolt-spot/src/main/java/com/cmex/bolt/spot/api/Message.java
+++ bolt-spot/src/main/java/com/cmex/bolt/spot/api/Message.java
@@ -10,15 +10,6 @@ public class Message extends Struct {
     public final Signed64 id = new Struct.Signed64();
     public final SpotEvent payload = inner(new SpotEvent());
 
-    public int getSize(Struct s) {
-        return (this.size() - payload.size()) + s.size();
-    }
-
-    public int getSize() {
-        EventType type = this.type.get();
-        return getSize(type.getStruct());
-    }
-
     @Override
     public String toString() {
         return "Message [type=" + type.get() + "]";
